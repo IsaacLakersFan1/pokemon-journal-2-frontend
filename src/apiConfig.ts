08974fa52f88757457666d6ec4a8b95fe6144552
Isaac Salazar <isaacsalazar9911@gmail.com>
2024-12-21 10:20:27 -0600

    Update url apiConfig

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,3 +1,4 @@
 // src/apiConfig.ts
-const API_BASE_URL = "http://localhost:3000";
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "http://i04k8wgokc0c804cwwsc4ggw.193.46.198.43.sslip.io";
 export default API_BASE_URL;
