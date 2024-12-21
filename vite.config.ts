60e94ec0453f66b06c7869c7d9d242c0df6f43b5
Isaac Salazar <isaacsalazar9911@gmail.com>
2024-12-21 10:12:01 -0600

    Add trafik config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // Binds to 0.0.0.0
+    port: 4173, // Ensures the port is correct
+  },
 });
